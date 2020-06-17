e88a2cda7ec31be5a48be511b486afb8f7e92c92
hvhq <64365465+hvhq@users.noreply.github.com>
2020-06-17 20:15:58 +0700

    Date, Convert, GetTime, WeekDay, CheckGregorianDate

--- main.asm
+++ main.asm
@@ -426,3 +426,404 @@ CopyStr:
 	jr $ra
 	
 	
+#char* Date(int day, int month, int year, char*TIME)
+#convert number day, month, year into string TIME
+#return address of TIME
+Date:
+	#save base 10 to register
+	addi $t1, $0, 10
+	
+	addi $t0, $0, 47	#$t0='/'
+	sb $t0, 2($a3)		#TIME[2]='/'
+	sb $t0, 5($a3)		#TIME[5]='/'
+	add $t0, $0, $0		#'\0'
+	sb $t0, 10($a3)		#TIME[10]='\0'
+	
+	#convert day to char
+	div $a0, $t1		#day=day/10
+	mflo $a0
+	mfhi $t0		#$t0=day%10
+	addi $t0, $t0, 48	#convert number to char
+	sb $t0, 1($a3)		#save byte to place hold by $s0 (TIME[1])
+	addi $t0, $a0, 48	#to char
+	sb $t0, 0($a3)		#save byte to TIME[0]
+
+	#convert month to char
+	div $a1, $t1		#month=month/10
+	mflo $a1
+	mfhi $t0		#$t0=month%10
+	addi $t0, $t0, 48	#number to char
+	sb $t0, 4($a3)		#
+	addi $t0, $a1, 48	#number to char
+	sb $t0, 3($a3)		#
+	
+	#convert year to char
+	div $a2, $t1		#year=year/10
+	mflo $a2
+	mfhi $t0		#$t0=year%10
+	addi $t0, $t0, 48	#number to char
+	sb $t0, 9($a3)		#
+	
+	div $a2, $t1		#year=year/10
+	mflo $a2
+	mfhi $t0		#$t0=year%10
+	addi $t0, $t0, 48	#number to char
+	sb $t0, 8($a3)		#
+	
+	div $a2, $t1		#year=year/10
+	mflo $a2
+	mfhi $t0		#$t0=year%10
+	addi $t0, $t0, 48	#number to char
+	sb $t0, 7($a3)		#
+	
+	addi $t0, $a2, 48	#number to char
+	sb $t0, 6($a3)		#
+	
+	#return value
+	add $v0, $a3, $0	
+	jr $ra
+
+#char* Convert(char*TIME, char type)
+#convert TIME format (DD/MM/YYYY) to:
+#type='A': MM/DD/YYYY
+#type='B': Month DD, YYYY
+#type='C': DD Month, YYYY
+#return address of TIME
+Convert:
+	addi $t0, $0, 65
+	bne $t0, $a1, TypeBC
+	TypeA:			#this label just for clear, no use
+		lb $t0, 0($a0)		#exchange first digit of month and day
+		lb $t1, 3($a0)
+		sb $t1, 0($a0)
+		sb $t0, 3($a0)
+		lb $t0, 1($a0)		#exchange second digit of month and day
+		lb $t1, 4($a0)
+		sb $t1, 1($a0)
+		sb $t0, 4($a0)
+		add $v0, $a0, $0
+		j EndConvert
+	TypeBC:
+		#shift right 10 character
+		addi $t1, $a0, 10
+		For1:
+			slt $t0, $a0, $t1
+			beq $t0, $0, ExitFor1
+			lb $t0, 0($a0)
+			sb $t0, 10($a0)
+			addi $a0, $a0, 1
+			j For1
+		ExitFor1:
+		addi $a0, $a0, -10	#TIME[0]
+		addi $t2, $0, 10
+		#get month to s2
+		lb $t1, 3($a0)		#first digit of month
+		addi $t1, $t1, -48	#character to number
+		mult $t1, $t2		#*10
+		mflo $t1
+		lb $t0, 4($a0)
+		addi $t0, $t0, -48	#character to number
+		add $t1, $t1, $t0
+		#find the pos begin of month in string monthname
+		addi $t1, $t1, -1
+		mult $t1, $t2		#*10, character-th
+		mflo $t1
+		la $t2, monthname	#add begin address, monthname[0]+k
+		add $t1, $t1, $t2	#&monthname[k]
+		
+		addi $sp, $sp, -4	#spare to save TIME[0]
+		sw $a0, 0($sp)		#save TIME[0]
+		#check type B or C
+		addi $t0, $0, 66	#'B'
+		beq $t0, $a1, AddMonth
+		addi $a0, $a0, 3	#case C: 'DD Month'=> begin add month at index 3
+		addi $t0, $0, 32	#' '
+		sb $t0, -1($a0)
+		AddMonth:
+			lb $t2, 0($t1)
+			sb $t2, 0($a0)
+			addi $t0, $t2, -32
+			beq $t0, $0, EndAddMonth
+			addi $t1, $t1, 1
+			addi $a0, $a0, 1
+			j AddMonth
+		EndAddMonth:
+		lw $t1, 0($sp)		#get back saved TIME[0]
+		addi $t1, $t1, 10	#TIME[10]
+		#check type B or C
+		addi $t0, $0, 66	#'B'
+		bne $t0, $a1, TypeC		
+	TypeB:
+		#shift day
+		lb $t0, 0($t1)
+		sb $t0, 1($a0)
+		lb $t0, 1($t1)
+		sb $t0, 2($a0)
+		addi $a0, $a0, 3	#don't jump, go next to Type C to get year on	
+	TypeC:
+		addi $t0, $0, 44	#','
+		sb $t0, 0($a0)
+		addi $t0, $0, 32	#' '
+		sb $t0, 1($a0)
+		#shift year
+		lb $t0, 6($t1)
+		sb $t0, 2($a0)
+		lb $t0, 7($t1)
+		sb $t0, 3($a0)
+		lb $t0, 8($t1)
+		sb $t0, 4($a0)
+		lb $t0, 9($t1)
+		sb $t0, 5($a0)
+		add $t0, $0, $0	#'\0'
+		sb $t0, 6($a0)
+
+		lw $v0, 0($sp)		#first $a0, &TIME[0]
+		addi $sp, $sp, 4	#Just in case BC, we spare $sp for use, so we move back
+	EndConvert:
+	jr $ra
+
+
+#int GetTime(char*TIME1, char*TIME2)
+#return the distance from TIME1 to TIME2, TIME1<TIME2
+GetTime:
+	addi $sp, $sp, -36
+	sw $ra, 0($sp)
+	sw $a0, 4($sp)
+	sw $a1, 8($sp)
+	sw $s0, 12($sp)
+	sw $s1, 16($sp)
+	sw $s2, 20($sp)
+	sw $s3, 24($sp)
+	sw $s4, 28($sp)
+	sw $s5, 32($sp)
+	
+	add $s5, $a0, $0
+	jal Year
+	add $s0, $v0, $0
+	
+	add $a0, $s5, $0
+	jal Month
+	add $s1, $v0, $0
+	
+	add $a0, $s5, $0
+	jal Day
+	add $s2, $v0, $0
+	
+	lw $s5, 8($sp)
+	add $a0, $s5, $0
+	jal Year
+	add $s3, $v0, $0
+	
+	add $a0, $s5, $0
+	jal Month
+	add $s4, $v0, $0
+	
+	add $a0, $s5, $0
+	jal Day
+	add $s5, $v0, $0
+	
+	#year diff->$s0
+	sub $s0, $s3, $s0
+
+	#if (month 2 < month 1)=>year diff-=1 
+	slt $t0, $s4, $s1
+	beq $t0, $0, MONTH2_NOT_SMALLER
+	addi $v0, $s0, -1
+	j END_GETTIME
+
+	MONTH2_NOT_SMALLER:
+	#if (month 1<month 2)=>year diff not change, return
+	slt $t0, $s1, $s4
+	beq $t0, $0, MONTH2_NOT_BIGGER
+	add $v0, $s0, $0
+	j END_GETTIME
+
+	MONTH2_NOT_BIGGER:
+	#(day 2 < day 1)? YES=> year-=1
+	slt $t0, $s5, $s2
+	beq $t0, $0, DAY2_NOT_SMALLER
+	#day 2 smaller: check if month ==2 and year2 is a leap year, and day 2 is 28
+	addi $s4, $0, 2
+	bne $s1, $s4, NOT_ENOUGH_DAY
+	addi $s4, $0, 28
+	bne $s5, $s4, NOT_ENOUGH_DAY
+	add $a0, $s3, $0
+	jal CheckLeapYear
+	bne $v0, $0, NOT_ENOUGH_DAY	#is a leap year=>not enough day
+	j DAY2_NOT_SMALLER
+	NOT_ENOUGH_DAY:
+	addi $v0, $s0, -1
+	j END_GETTIME
+	DAY2_NOT_SMALLER:
+	add $v0, $s0, $0	
+	
+	END_GETTIME:
+	lw $ra, 0($sp)
+	lw $a0, 4($sp)
+	lw $a1, 8($sp)
+	lw $s0, 12($sp)
+	lw $s1, 16($sp)
+	lw $s2, 20($sp)
+	lw $s3, 24($sp)
+	lw $s4, 28($sp)
+	lw $s5, 32($sp)
+	addi $sp, $sp, 36
+	jr $ra
+
+#int WeekDay(char* TIME)
+#return the day of the week (0,1,2,3,4,5,6)->(Sun, Mon, Tue, Wed, Thu, Fri, Sar)
+WeekDay:
+	addi $sp, $sp, -20
+	sw $ra, 0($sp)
+	sw $s0, 4($sp)
+	sw $s1, 8($sp)
+	sw $s2, 12($sp)
+	sw $s3, 16($sp)
+	
+	add $s0, $a0, $0	#&TIME[0]
+	jal Day
+	add $s1, $v0, $0	#add d
+	
+	add $a0, $s0, $0
+	jal Year
+	add $s2, $v0, $0
+	
+	add $a0, $s0, $0
+	jal CheckGregorianDate
+	
+	addi $t0, $0, 100
+	div $s2, $t0
+	mflo $t0
+	mfhi $t1
+	
+	#add c
+	#if v0==0 => Julian Date
+	beq $v0, $0, JULIAN_DATE
+	#if gregorian date
+	addi $sp, $sp, -28
+	addi $t2, $0, 4
+	sw $t2, 0($sp)
+	addi $t2, $0, 2
+	sw $t2, 4($sp)
+	addi $t2, $0, 0
+	sw $t2, 8($sp)
+	addi $t2, $0, 6
+	sw $t2, 12($sp)
+	addi $t2, $0, 4
+	sw $t2, 16($sp)
+	addi $t2, $0, 2
+	sw $t2, 20($sp)
+	addi $t2, $0, 0
+	sw $t2, 24($sp)
+	
+	addi $t0, $t0, -17	#century=17->index 0
+	sll $t0, $t0, 2
+	add $t0, $t0, $sp
+	lw $t0, 0($t0)
+	add $s1, $s1, $t0
+	addi $sp, $sp, 28
+	j ADD_YEAR
+	
+	#if julian date
+	JULIAN_DATE:
+	addi $t2, $0, 18
+	sub $t2, $t2, $t0
+	add $s1, $s1, $t2
+	
+	ADD_YEAR:
+	add $s1, $s1, $t1	#add y
+	
+	addi $t0, $0, 4
+	div $t1, $t0
+	mflo $t0
+	add $s1, $s1, $t0	#add floor(y/4)
+	
+	add $a0, $s0, $0
+	jal Month
+	add $s3, $v0, $0	#save month
+	
+	addi $sp, $sp, -48
+	addi $t0, $0, 0
+	sw $t0, 0($sp)
+	addi $t0, $0, 3
+	sw $t0, 4($sp)
+	addi $t0, $0, 3
+	sw $t0, 8($sp)
+	addi $t0, $0, 6
+	sw $t0, 12($sp)
+	addi $t0, $0, 1
+	sw $t0, 16($sp)
+	addi $t0, $0, 4
+	sw $t0, 20($sp)
+	addi $t0, $0, 6
+	sw $t0, 24($sp)
+	addi $t0, $0, 2
+	sw $t0, 28($sp)
+	addi $t0, $0, 5
+	sw $t0, 32($sp)
+	addi $t0, $0, 0
+	sw $t0, 36($sp)
+	addi $t0, $0, 3
+	sw $t0, 40($sp)
+	addi $t0, $0, 5
+	sw $t0, 44($sp)
+
+	add $a0, $s2, $0
+	jal CheckLeapYear
+	beq $v0, $0, NOT_LEAP_YEAR
+	addi $t0, $0, 6
+	sw $t0, 0($sp)
+	addi $t0, $0, 2
+	sw $t0, 4($sp)		#do not jump
+	NOT_LEAP_YEAR:
+	
+	add $s2, $sp, $0	#begin pos of m array
+	addi $s3, $s3, -1
+	sll $s3, $s3, 2
+	add $s2, $s2, $s3	#pos of particular m element needed
+	lw $t0, 0($s2)		#load m
+	add $s1, $s1, $t0	#add m
+	
+	addi $sp, $sp, 48
+	
+	addi $s2, $0, 7
+	div $s1, $s2
+	mfhi $v0
+	
+	lw $ra, 0($sp)
+	lw $s0, 4($sp)
+	lw $s1, 8($sp)
+	lw $s2, 12($sp)
+	lw $s3, 16($sp)
+	addi $sp, $sp, 20
+	jr $ra
+#int CheckGregorianDate(char* TIME)
+#return 1 if TIME is in Gregorian Date, others it will return 0 (include 3/9/1752->13/9/1752).
+CheckGregorianDate:
+	addi $sp, $sp, -8
+	sw $ra, 0($sp)
+	sw $a0, 4($sp)
+	
+	jal Year
+	addi $t0, $0, 1752
+	slt $t0, $v0, $t0
+	bne $t0, $0, CheckGregorianDate_RETURN_0
+	
+	lw $a0, 4($sp)
+	jal Day
+	addi $t0, $0, 14
+	slt $t0, $v0, $t0
+	bne $t0, $0, CheckGregorianDate_RETURN_0
+	
+	j CheckGregorianDate_RETURN_1
+	
+	CheckGregorianDate_RETURN_0:
+	add $v0, $0, $0
+	j CheckGregorianDate_END
+	CheckGregorianDate_RETURN_1:
+	addi $v0, $0, 1
+	CheckGregorianDate_END:
+	lw $ra, 0($sp)
+	addi, $sp, $sp, 8
+	jr $ra
+	
